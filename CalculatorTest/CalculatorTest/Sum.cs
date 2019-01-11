bbddb700209c624440619328adb86a50c193b9ae
ramrajnkl <rammraj.nkl@gmail.com>
2019-01-11 09:11:10 -0800

    Initial checkin..

---
+++ CalculatorTest/CalculatorTest/Sum.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace CalculatorTest
+{
+
+    public class Sum
+    {
+        /// <summary>
+        /// Addition Test
+        /// </summary>
+        [Test]
+        public void AdditionTest()
+        {
+            int a = 10;
+            int b = 10;
+
+            int sum = a + b;
+
+            Assert.AreEqual(sum, 20);
+        }
+
+        /// <summary>
+        /// Subtraction Test
+        /// </summary>
+        [Test]
+        public void SubtractionTest()
+        {
+            int a = 100;
+            int b = 50;
+
+            int sum = a - b;
+
+            Assert.AreEqual(sum, 50);
+        }
+    }
+}
